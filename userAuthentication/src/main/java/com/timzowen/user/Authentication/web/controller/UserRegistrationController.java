57f4f420873b12a60332de108e9911a4253129ce
TimzOwen <timzowen@gmail.com>
2022-09-27 12:54:09 +0300

    registration controller

--- userAuthentication/src/main/java/com/timzowen/user/Authentication/web/controller/UserRegistrationController.java
+++ userAuthentication/src/main/java/com/timzowen/user/Authentication/web/controller/UserRegistrationController.java
@@ -1,10 +1,25 @@
 package com.timzowen.user.Authentication.web.controller;
 
+import com.timzowen.user.Authentication.service.UserService;
+import com.timzowen.user.Authentication.web.dto.UserRegistrationDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/registration")
 public class UserRegistrationController {
+
+    private UserService userService;
+
+    public UserRegistrationController(UserService userService) {
+        super();
+        this.userService = userService;
+    }
+
+    public String registerUserAccount(@ModelAttribute("user")UserRegistrationDto registrationDto){
+        userService.save(registrationDto);
+        return "redirect:/registration?success";
+    }
 
 }
