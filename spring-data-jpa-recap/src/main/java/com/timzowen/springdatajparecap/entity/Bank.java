902b5c946428ac4bda4225841736e347c2c87536
TimzOwen <timzowen@gmail.com>
2022-10-05 11:02:32 +0300

    lombok & entitiy

--- spring-data-jpa-recap/src/main/java/com/timzowen/springdatajparecap/entity/Bank.java
+++ spring-data-jpa-recap/src/main/java/com/timzowen/springdatajparecap/entity/Bank.java
@@ -1,15 +1,38 @@
 package com.timzowen.springdatajparecap.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "banks")
 public class Bank {
     @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "banks_gen"
+    )
+    @SequenceGenerator(
+            name = "banks_gen",
+            sequenceName = "banks_seq_gen",
+            allocationSize = 1
+    )
+
     private int id;
+    @Column(name = "bank_name")
     private String name;
+    @Column(name = "bank_location")
     private String location;
+    @Column(name = "total_employees")
     private int employees;
+    @Column(name = "bank_net_worth")
     private BigDecimal netWorth;
 }
