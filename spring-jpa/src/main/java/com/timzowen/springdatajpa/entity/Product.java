2f03c15f6c6709f38f31b7606a17c4f3b8c4ac42
TimzOwen <timzowen@gmail.com>
2022-10-05 09:37:35 +0300

    annotation update

--- spring-jpa/src/main/java/com/timzowen/springdatajpa/entity/Product.java
+++ spring-jpa/src/main/java/com/timzowen/springdatajpa/entity/Product.java
@@ -1,21 +1,45 @@
 package com.timzowen.springdatajpa.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "products",
+        schema = "ecommerce",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "stock_unit",
+                        columnNames = "sku"
+                ),
+                @UniqueConstraint(name = "products_prices",
+                        columnNames = "price")
+        })
 public class Product {
     @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_gen"
+    )
+    @SequenceGenerator(
+            name = "product_gen",
+            sequenceName = "product_seq_name",
+            allocationSize = 2
+    )
     private Long id;
     private String sku;
+    @Column(name = "product_name")
     private String name;
     private String description;
     private BigDecimal price;
     private boolean active;
     private String imageUrl;
+    @CreationTimestamp
     private LocalDateTime dateCreated;
+    @UpdateTimestamp
     private LocalDateTime lastUpdated;
 
     // getters and setters
