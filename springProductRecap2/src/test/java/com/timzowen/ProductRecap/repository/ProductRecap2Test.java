2c98895bc17038d98e4b06b913c4ca3fa77a62c6
TimzOwen <timzowen@gmail.com>
2022-10-09 19:49:35 +0300

    tests

--- springProductRecap2/src/test/java/com/timzowen/ProductRecap/repository/ProductRecap2Test.java
+++ springProductRecap2/src/test/java/com/timzowen/ProductRecap/repository/ProductRecap2Test.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @SpringBootTest
 class ProductRecap2Test {
@@ -22,6 +23,29 @@ class ProductRecap2Test {
         product.setCost(new BigDecimal("67000.500"));
 
         productRepository.save(product);
+    }
+    @Test
+    void saveAllMethod(){
+
+        Product product = new Product();
+        product.setName("Table");
+        product.setLocation("Eldoret");
+        product.setManufacturer("Mahogany");
+        product.setCost(new BigDecimal("12000.500"));
+
+        Product product1 = new Product();
+        product1.setName("Tri-circle");
+        product1.setLocation("Nairobi");
+        product1.setManufacturer("Tools Co");
+        product1.setCost(new BigDecimal("609383.500"));
+
+        Product product2 = new Product();
+        product2.setName("Speakers");
+        product2.setLocation("Embu");
+        product2.setManufacturer("Sony");
+        product2.setCost(new BigDecimal("883456.500"));
+
+        productRepository.saveAll(List.of(product,product1,product2));
 
     }
 
