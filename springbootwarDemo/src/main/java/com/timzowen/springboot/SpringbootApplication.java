35e34991d981db4b3995a8db75ff9970afb81718
TimzOwen <timzowen@gmail.com>
2022-10-10 22:05:46 +0300

    depoying as war extenally

---
+++ springbootwarDemo/src/main/java/com/timzowen/springboot/SpringbootApplication.java
@@ -0,0 +1,21 @@
+package com.timzowen.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@GetMapping("/test")
+	public String testCheck(){
+		return "Test Ok";
+	}
+
+}
