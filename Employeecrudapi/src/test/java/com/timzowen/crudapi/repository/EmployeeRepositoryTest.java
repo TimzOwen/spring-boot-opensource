34a81a76941d0a75cc96ca3f62bf595d3ad12d81
TimzOwen <timzowen@gmail.com>
2022-10-11 00:48:56 +0300

    test update

--- Employeecrudapi/src/test/java/com/timzowen/crudapi/repository/EmployeeRepositoryTest.java
+++ Employeecrudapi/src/test/java/com/timzowen/crudapi/repository/EmployeeRepositoryTest.java
@@ -4,6 +4,8 @@ import com.timzowen.crudapi.entity.Employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
