9e8f0b666234994926d558369bbc3cb595e85e75
TimzOwen <timzowen@gmail.com>
2022-10-11 00:53:39 +0300

    crud ecommerce

---
+++ ecommercecurapi/src/main/java/com/timzowen/ecommercecurapi/EcommercecurapiApplication.java
@@ -0,0 +1,13 @@
+package com.timzowen.ecommercecurapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercecurapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercecurapiApplication.class, args);
+	}
+
+}
---
+++ ecommercecurapi/src/test/java/com/timzowen/ecommercecurapi/EcommercecurapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timzowen.ecommercecurapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercecurapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
