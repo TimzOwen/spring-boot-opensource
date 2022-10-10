189ec7584e0f19bcba87c2bbb5f4d0ae62ced1a8
TimzOwen <timzowen@gmail.com>
2022-10-10 12:59:14 +0300

    initial commit

---
+++ hellospringboot/src/test/java/com/timzowen/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timzowen.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
