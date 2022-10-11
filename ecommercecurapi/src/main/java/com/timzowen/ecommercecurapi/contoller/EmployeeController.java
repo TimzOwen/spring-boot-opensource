1859fe70e9265b735d1dd7fba6e3565e7194781b
TimzOwen <timzowen@gmail.com>
2022-10-11 22:51:47 +0300

    .

--- ecommercecurapi/src/main/java/com/timzowen/ecommercecurapi/contoller/EmployeeController.java
+++ ecommercecurapi/src/main/java/com/timzowen/ecommercecurapi/contoller/EmployeeController.java
@@ -1,9 +1,7 @@
 package com.timzowen.ecommercecurapi.contoller;
 
 import com.timzowen.ecommercecurapi.model.Employee;
-import com.timzowen.ecommercecurapi.repository.EmployeeRepository;
 import com.timzowen.ecommercecurapi.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- ecommercecurapi/src/main/java/com/timzowen/ecommercecurapi/exception/ResourceNotFound.java
+++ ecommercecurapi/src/main/java/com/timzowen/ecommercecurapi/exception/ResourceNotFound.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class ResourceNotFound extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
-    private String firstName;
-    private String fieldName;
-    private Object fieldValue;
+    private final String firstName;
+    private final String fieldName;
+    private final Object fieldValue;
 
-    public ResourceNotFound(String firstName, String fieldName, Object fieldValue) {
+    public ResourceNotFound(String firstName, String fieldName, Object fieldValue){
         super(String.format("%s not found with %s : %s ", firstName,fieldName,fieldValue));
         this.firstName = firstName;
         this.fieldName = fieldName;
