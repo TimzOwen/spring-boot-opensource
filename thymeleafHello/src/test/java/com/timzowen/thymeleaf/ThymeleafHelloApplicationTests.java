70b884bc3f31c46206a12b79c9cf2b42776dbbea
TimzOwen <timzowen@gmail.com>
2022-10-13 09:48:29 +0300

    thymeleaf

---
+++ thymeleafHello/src/test/java/com/timzowen/thymeleaf/ThymeleafHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timzowen.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
