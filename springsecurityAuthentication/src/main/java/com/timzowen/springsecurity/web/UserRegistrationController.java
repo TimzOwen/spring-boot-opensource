bab730f6542afe9972701bea5d936f20d0d94a4c
TimzOwen <timzowen@gmail.com>
2022-10-21 21:21:21 +0300

    rgistraion mapping of url to template

--- springsecurityAuthentication/src/main/java/com/timzowen/springsecurity/web/UserRegistrationController.java
+++ springsecurityAuthentication/src/main/java/com/timzowen/springsecurity/web/UserRegistrationController.java
@@ -1,17 +1,29 @@
 package com.timzowen.springsecurity.web;
 
 import com.timzowen.springsecurity.service.impl.UserServiceImpl;
+import com.timzowen.springsecurity.web.dto.UserRegistrationDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/registration")
 public class UserRegistrationController {
-
     private UserServiceImpl userService;
-
 
     public UserRegistrationController(UserServiceImpl userService) {
         this.userService = userService;
     }
+
+    @GetMapping("/registration")
+    public String showRegistration(){
+        return "registration";
+    }
+
+    public String saveUserAccount(@ModelAttribute("user")UserRegistrationDto registrationDto){
+        userService.save(registrationDto);
+        return "redirect:/registration?success";
+    }
+
 }
