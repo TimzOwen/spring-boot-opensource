400da579d3ddb3d2fdaf5c9f72f79eb18bd77b2f
TimzOwen <timzowen@gmail.com>
2022-11-04 18:05:49 +0300

    spring Rest APIS demo using spring boot

---
+++ spring-student-demo/src/test/java/com/timzowen/spring/student/demo/SpringStudentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timzowen.spring.student.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
