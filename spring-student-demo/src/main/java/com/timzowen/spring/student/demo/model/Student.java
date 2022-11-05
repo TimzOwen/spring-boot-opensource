ac4f1366cd53e0df6e9aa2115731c5c85cef72c4
TimzOwen <timzowen@gmail.com>
2022-11-06 00:23:52 +0300

    database configurations update on application profiles

--- spring-student-demo/src/main/java/com/timzowen/spring/student/demo/model/Student.java
+++ spring-student-demo/src/main/java/com/timzowen/spring/student/demo/model/Student.java
@@ -1,6 +1,11 @@
 package com.timzowen.spring.student.demo.model;
 
+import lombok.Data;
+
+
+@Data
 public class Student {
+    private Long Id;
     private String firstName;
     private String lastName;
     private String email;
