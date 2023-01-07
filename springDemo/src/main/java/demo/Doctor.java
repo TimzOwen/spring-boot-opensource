43652f06bd5f2e052bd90e503fb6f440bab84d23
TimzOwen <timzowen@gmail.com>
2023-01-07 16:21:11 +0300

    demo for bean injection

---
+++ springDemo/src/main/java/demo/Doctor.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Doctor {
+
+    public void assist(){
+
+        System.out.println("Doctor in charge........");
+    }
+
+}
---
+++ springDemo/src/main/java/demo/Main.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.print.Doc;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        Doctor doctor = context.getBean(Doctor.class);
+        doctor.assist();
+
+    }
+}
