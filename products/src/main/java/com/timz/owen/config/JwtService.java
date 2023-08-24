32a9c3a0ff3c9bff876443a1464306285d520d03
TimzOwen <timzowen@gmail.com>
2023-08-24 22:48:08 +0300

    jwt dependecy updates

--- products/src/main/java/com/timz/owen/config/JwtService.java
+++ products/src/main/java/com/timz/owen/config/JwtService.java
@@ -1,10 +1,25 @@
 package com.timz.owen.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
     public String extractUserName(String token) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private byte[] getSigningKey() {
         return null;
     }
 }
