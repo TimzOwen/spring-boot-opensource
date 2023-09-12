3f7431ffa39ea6a3e0a1c7bd8bad024ce4f61a0d
TimzOwen <timzowen@gmail.com>
2023-09-12 20:55:23 +0300

    yml and flyway configuration for DB

---
+++ reactive/src/main/java/com/timzowen/reactive/student/Student.java
@@ -0,0 +1,19 @@
+package com.timzowen.reactive.student;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "students")
+public class Student {
+    @Id
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private int age;
+}
