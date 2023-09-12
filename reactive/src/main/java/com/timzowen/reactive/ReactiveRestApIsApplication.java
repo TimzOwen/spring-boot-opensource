a730a9ae7cfcd60d91fc099ee6710affab92a4b7
TimzOwen <timzowen@gmail.com>
2023-09-12 20:28:33 +0300

    setup for reactive dependencies

---
+++ reactive/src/main/java/com/timzowen/reactive/ReactiveRestApIsApplication.java
@@ -0,0 +1,13 @@
+package com.timzowen.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRestApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveRestApIsApplication.class, args);
+	}
+
+}
---
+++ reactive/src/test/java/com/timzowen/reactive/ReactiveRestApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timzowen.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRestApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
