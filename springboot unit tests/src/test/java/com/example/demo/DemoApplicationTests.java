b4f6e26517619f3eba1f698ded238a8bfb9aa08a
TimzOwen <timzowen@gmail.com>
2023-09-13 22:04:08 +0300

    set aserj and test

--- springboot unit tests/src/test/java/com/example/demo/DemoApplicationTests.java
+++ springboot unit tests/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class DemoApplicationTests {
 
+	Calculator underTest = new Calculator();
 	@Test
-	void contextLoads() {
+	void addNumbers() {
+		int result = underTest.add(20,30);
+		assertThat(result).isEqualTo(30 + 20);
 	}
 
+	static class Calculator{
+		int add(int a, int b) {
+			return a + b;
+		}
+	}
 }
