94cf2792dc2cb4fd9b4b3053025ed9ef96d53b85
TimzOwen <timzowen@gmail.com>
2024-02-05 20:39:29 +0300

    TDD estore user service

---
+++ UserService/src/main/java/org/timzowen/estore/service/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package org.timzowen.estore.service;
+
+public class UserServiceImpl implements UserService {
+}
---
+++ UserService/src/test/java/org/timzowen/estore/service/UserServiceTest.java
@@ -0,0 +1,9 @@
+package org.timzowen.estore.service;
+
+public class UserServiceTest {
+
+    void testCreateUser_whenUserDetailsProvided_returnUserObject(){
+        UserService userService = new UserServiceImpl();
+    }
+
+}
