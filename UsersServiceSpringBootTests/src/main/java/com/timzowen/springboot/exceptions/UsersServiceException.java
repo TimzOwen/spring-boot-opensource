03de93e7b921677b2d4b88db44524175a63fc606
TimzOwen <timzowen@gmail.com>
2024-02-06 17:18:13 +0300

    refactor

--- UsersServiceSpringBootTests/src/main/java/com/timzowen/springboot/exceptions/UsersServiceException.java
+++
@@ -1,8 +0,0 @@
-package com.timzowen.springboot.exceptions;
-
-public class UsersServiceException extends RuntimeException{
-    public UsersServiceException(String message)
-    {
-        super(message);
-    }
-}
