0963508296a188a774e39e978039e4d89b29e8cc
TimzOwen <timzowen@gmail.com>
2024-02-07 17:39:40 +0300

    User login validation JWT token

--- UsersServiceIntegrationTest/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UserControllerWebLayerTest.java
+++ UsersServiceIntegrationTest/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UserControllerWebLayerTest.java
@@ -1,124 +1,124 @@
-package com.appsdeveloperblog.tutorials.junit.ui.controllers;
-
-import com.appsdeveloperblog.tutorials.junit.service.UsersService;
-import com.appsdeveloperblog.tutorials.junit.service.UsersServiceImpl;
-import com.appsdeveloperblog.tutorials.junit.shared.UserDto;
-import com.appsdeveloperblog.tutorials.junit.ui.request.UserDetailsRequestModel;
-import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.catalina.security.SecurityConfig;
-import org.junit.jupiter.api.*;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import java.util.UUID;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-
-@WebMvcTest(controllers = UsersController.class,
-excludeAutoConfiguration = {SecurityException.class})
-//@ContextConfiguration(classes = SecurityConfig.class)
-//@AutoConfigureMockMvc(addFilters = false)
-//@AutoConfigureMockMvc(addFilters = false) --> disable security scan
-//@MockBean({UsersServiceImpl.class})
-public class UserControllerWebLayerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    //@Autowired
-    @MockBean
-    UsersService usersService;
-
-    private UserDetailsRequestModel userDetailsRequestModel;
-
-    @BeforeEach
-    void setUP(){
-        userDetailsRequestModel = new UserDetailsRequestModel();
-        userDetailsRequestModel.setFirstName("timz");
-        userDetailsRequestModel.setLastName("owen");
-        userDetailsRequestModel.setEmail("timz@gmail.com");
-        userDetailsRequestModel.setPassword("12345678");
-        userDetailsRequestModel.setRepeatPassword("12345678");
-
-    }
-
-    @Test
-    @DisplayName("user can be created")
-    void testUserCreate_whenValidUserDetailsProvided_returnsCreatedUserDetails() throws Exception {
-        //Arrange
-
-
-        UserDto userDto = new ModelMapper().map(userDetailsRequestModel, UserDto.class);
-        userDto.setUserId(UUID.randomUUID().toString());
-        when(usersService.createUser(any(UserDto.class))).thenReturn(userDto);
-
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/users")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(userDetailsRequestModel));
-
-        // Act
-        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
-        String responseBodyAsString = mvcResult.getResponse().getContentAsString();
-        UserRest createdUser = new ObjectMapper().readValue(responseBodyAsString, UserRest.class);
-
-        //Assert
-        Assertions.assertEquals(userDetailsRequestModel.getFirstName(), createdUser.getFirstName(), "Returned first name incorrect");
-        Assertions.assertEquals(userDetailsRequestModel.getLastName(), createdUser.getLastName(), "Returned last name incorrect");
-        Assertions.assertEquals(userDetailsRequestModel.getEmail(), createdUser.getEmail(), "Returned email incorrect");
-        Assertions.assertFalse(createdUser.getUserId().isEmpty(), "user Id should not be empty");
-    }
-
-    @Test
-    @DisplayName("First Name is not empty")
-    void testCreateUser_WhenFirstNameIsEmpty_returns400StatusCode() throws Exception {
-        //Arrange
-        userDetailsRequestModel.setFirstName(" ");
-
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/users")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(userDetailsRequestModel));
-
-        // Act
-        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
-
-        // Assert
-        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), mvcResult.getResponse().getStatus(),
-                "Incorrect HTTP status code");
-
-    }
-
-    @Test
-    @DisplayName("firstName cannot be < 2 characters")
-    void testCreateUser_whenFirstNameIsOnlyOneCharacter_returns400StatusCode() throws Exception{
-        //Arrange
-        userDetailsRequestModel.setFirstName("abc");
-
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/users")
-                .content(new ObjectMapper().writeValueAsString(userDetailsRequestModel))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON);
-
-        // Act
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-
-        // Assert
-        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(),result.getResponse().getStatus(),
-                "HTTP status is not set to 400");
-    }
-
-}
+//package com.appsdeveloperblog.tutorials.junit.ui.controllers;
+//
+//import com.appsdeveloperblog.tutorials.junit.service.UsersService;
+//import com.appsdeveloperblog.tutorials.junit.service.UsersServiceImpl;
+//import com.appsdeveloperblog.tutorials.junit.shared.UserDto;
+//import com.appsdeveloperblog.tutorials.junit.ui.request.UserDetailsRequestModel;
+//import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.apache.catalina.security.SecurityConfig;
+//import org.junit.jupiter.api.*;
+//import org.modelmapper.ModelMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MvcResult;
+//import org.springframework.test.web.servlet.RequestBuilder;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//
+//import java.util.UUID;
+//
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.Mockito.when;
+//
+//@WebMvcTest(controllers = UsersController.class,
+//excludeAutoConfiguration = {SecurityException.class})
+////@ContextConfiguration(classes = SecurityConfig.class)
+////@AutoConfigureMockMvc(addFilters = false)
+////@AutoConfigureMockMvc(addFilters = false) --> disable security scan
+////@MockBean({UsersServiceImpl.class})
+//public class UserControllerWebLayerTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    //@Autowired
+//    @MockBean
+//    UsersService usersService;
+//
+//    private UserDetailsRequestModel userDetailsRequestModel;
+//
+//    @BeforeEach
+//    void setUP(){
+//        userDetailsRequestModel = new UserDetailsRequestModel();
+//        userDetailsRequestModel.setFirstName("timz");
+//        userDetailsRequestModel.setLastName("owen");
+//        userDetailsRequestModel.setEmail("timz@gmail.com");
+//        userDetailsRequestModel.setPassword("12345678");
+//        userDetailsRequestModel.setRepeatPassword("12345678");
+//
+//    }
+//
+//    @Test
+//    @DisplayName("user can be created")
+//    void testUserCreate_whenValidUserDetailsProvided_returnsCreatedUserDetails() throws Exception {
+//        //Arrange
+//
+//
+//        UserDto userDto = new ModelMapper().map(userDetailsRequestModel, UserDto.class);
+//        userDto.setUserId(UUID.randomUUID().toString());
+//        when(usersService.createUser(any(UserDto.class))).thenReturn(userDto);
+//
+//        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/users")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .accept(MediaType.APPLICATION_JSON)
+//                .content(new ObjectMapper().writeValueAsString(userDetailsRequestModel));
+//
+//        // Act
+//        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+//        String responseBodyAsString = mvcResult.getResponse().getContentAsString();
+//        UserRest createdUser = new ObjectMapper().readValue(responseBodyAsString, UserRest.class);
+//
+//        //Assert
+//        Assertions.assertEquals(userDetailsRequestModel.getFirstName(), createdUser.getFirstName(), "Returned first name incorrect");
+//        Assertions.assertEquals(userDetailsRequestModel.getLastName(), createdUser.getLastName(), "Returned last name incorrect");
+//        Assertions.assertEquals(userDetailsRequestModel.getEmail(), createdUser.getEmail(), "Returned email incorrect");
+//        Assertions.assertFalse(createdUser.getUserId().isEmpty(), "user Id should not be empty");
+//    }
+//
+//    @Test
+//    @DisplayName("First Name is not empty")
+//    void testCreateUser_WhenFirstNameIsEmpty_returns400StatusCode() throws Exception {
+//        //Arrange
+//        userDetailsRequestModel.setFirstName(" ");
+//
+//        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/users")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .accept(MediaType.APPLICATION_JSON)
+//                .content(new ObjectMapper().writeValueAsString(userDetailsRequestModel));
+//
+//        // Act
+//        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+//
+//        // Assert
+//        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), mvcResult.getResponse().getStatus(),
+//                "Incorrect HTTP status code");
+//
+//    }
+//
+//    @Test
+//    @DisplayName("firstName cannot be < 2 characters")
+//    void testCreateUser_whenFirstNameIsOnlyOneCharacter_returns400StatusCode() throws Exception{
+//        //Arrange
+//        userDetailsRequestModel.setFirstName("abc");
+//
+//        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/users")
+//                .content(new ObjectMapper().writeValueAsString(userDetailsRequestModel))
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .accept(MediaType.APPLICATION_JSON);
+//
+//        // Act
+//        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+//
+//        // Assert
+//        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(),result.getResponse().getStatus(),
+//                "HTTP status is not set to 400");
+//    }
+//
+//}
--- UsersServiceIntegrationTest/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ UsersServiceIntegrationTest/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,27 +1,25 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import com.appsdeveloperblog.tutorials.junit.security.SecurityConstants;
 import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.web.client.RestTemplate;
 
-import javax.validation.constraints.AssertFalse;
 import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //@TestPropertySource(locations = "/application-test.properties",
 //properties = "server.port=8082")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UsersControllerIntegrationTest{
 
     @Value("${server.port}")
@@ -35,6 +33,7 @@ public class UsersControllerIntegrationTest{
 
    @Test
    @DisplayName("user can be created")
+   @Order(1)
     void testCreateUser_whenValidDetailsProvided_returnsUserDetails() throws JSONException {
        //Arrange
        JSONObject userDetailsRequestJson = new JSONObject();
@@ -66,6 +65,48 @@ public class UsersControllerIntegrationTest{
        Assertions.assertEquals(userDetailsRequestJson.getString("email"),
                createdUserDetails.getEmail(),"Returned user's email seems to be incorrect");
        Assertions.assertFalse(createdUserDetails.getUserId().trim().isEmpty(),"user Id should not be empty");
+   }
+
+   @Test
+    @DisplayName("GET / users requires JWT")
+   @Order(2)
+    void testGetUsers_whenMissingJWT_return403(){
+       // Arrange
+       HttpHeaders headers = new HttpHeaders();
+       headers.set("Accept","application/json");
+
+       HttpEntity requestEntity = new HttpEntity(null,headers);
+
+       //Act
+       ResponseEntity<List<UserRest>> response = testRestTemplate.exchange("/users", HttpMethod.GET, requestEntity,
+               new ParameterizedTypeReference<List<UserRest>>() {
+               });
+
+       // Assert
+       Assertions.assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode(),"Http status code 403 should have returned");
+   }
+
+   @Test
+    @DisplayName("/Login works")
+   @Order(3)
+    void testUserLogin_whenCorrectCredProvided_returnJWTinAuthTokenHeader() throws JSONException {
+       //Arrange
+       JSONObject loginCredentials = new JSONObject();
+       loginCredentials.put("email","timz@gmail.com");
+       loginCredentials.put("password","12345678");
+
+       HttpEntity<String> request = new HttpEntity<>(loginCredentials.toString());
+
+       //Act
+       ResponseEntity response = testRestTemplate.postForEntity("/users/login",request,null);
+
+       //Assert
+       Assertions.assertEquals(HttpStatus.OK,response.getStatusCode(),"Http status should be 200");
+       Assertions.assertNotNull(response.getHeaders().getValuesAsList(SecurityConstants.HEADER_STRING).get(0),
+               "Response should contain Authorization with JWT");
+       Assertions.assertNotNull(response.getHeaders().getValuesAsList("UserID").get(0),
+               "response should contain user in response header Id ");
 
    }
+
 }
