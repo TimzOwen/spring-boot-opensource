4f374a3b80d224158b2d51570e069424c1adea31
TimzOwen <timzowen@gmail.com>
2024-02-07 12:23:08 +0300

    random port assignments while running springboot

--- UsersServiceIntegrationTest/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ UsersServiceIntegrationTest/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -4,19 +4,24 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@TestPropertySource(locations = "/application-test.properties",
-properties = "server.port=8082")
-public class UsersControllerIntegrationTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@TestPropertySource(locations = "/application-test.properties",
+//properties = "server.port=8082")
+public class UsersControllerIntegrationTest{
 
     @Value("${server.port}")
     private int serverPort;
+
+    @LocalServerPort
+    private int localServerPort;
 
     @Test
     void contextLoads(){
         System.out.println(" defining port in properties and using @SBT annotation");
         System.out.println("server port: " + serverPort);
+        System.out.println("local port: " + localServerPort);
     }
 }
