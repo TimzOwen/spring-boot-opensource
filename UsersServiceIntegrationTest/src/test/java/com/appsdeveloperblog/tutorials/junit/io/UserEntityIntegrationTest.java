6ee5833fecb1287c615a40ca54a1bf8af59529a7
TimzOwen <timzowen@gmail.com>
2024-02-07 23:43:27 +0300

    catch unique id assignments

--- UsersServiceIntegrationTest/src/test/java/com/appsdeveloperblog/tutorials/junit/io/UserEntityIntegrationTest.java
+++ UsersServiceIntegrationTest/src/test/java/com/appsdeveloperblog/tutorials/junit/io/UserEntityIntegrationTest.java
@@ -3,15 +3,18 @@ package com.appsdeveloperblog.tutorials.junit.io;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import javax.persistence.EntityManager;
 import javax.persistence.PersistenceException;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @DataJpaTest
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserEntityIntegrationTest {
     private UserEntity userEntity;
 
@@ -49,9 +52,26 @@ public class UserEntityIntegrationTest {
         userEntity.setFirstName("TimzTimzTimzTimzTimzTimzTimzTimzTimzTimzTimzTimzTimzTimzTimzTimzTimzTimzTimz");
 
         //Act & assert
-        Assertions.assertThrows(PersistenceException.class,()->{
+        assertThrows(PersistenceException.class,()->{
             testEntityManager.persistAndFlush(userEntity);
         },"Expected Persistence Exception to tbe thrown");
-
     }
+
+    @Test
+    void testUserEntity_whenDuplicateUserIdExist_shouldThrowException(){
+        UserEntity newUserEntity = new UserEntity();
+        newUserEntity.setUserId("1");
+        newUserEntity.setFirstName("Timz");
+        newUserEntity.setLastName("Owen");
+        newUserEntity.setEmail("timz@gmail.com");
+        newUserEntity.setEncryptedPassword("12345678");
+        testEntityManager.persistAndFlush(newUserEntity);
+
+        userEntity.setUserId("1");
+
+        assertThrows(PersistenceException.class, ()-> {
+            testEntityManager.persistAndFlush(userEntity);
+        },"Expected Persistence Exception to be throw.");
+    }
+
 }
