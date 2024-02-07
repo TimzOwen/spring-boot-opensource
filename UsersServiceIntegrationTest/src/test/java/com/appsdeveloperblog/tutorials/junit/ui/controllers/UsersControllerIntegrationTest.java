b8c9f9e6f544bd539ee45970a59b78f95d695561
TimzOwen <timzowen@gmail.com>
2024-02-07 15:50:15 +0300

    testing json passed objects

--- UsersServiceIntegrationTest/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ UsersServiceIntegrationTest/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,11 +1,23 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.client.RestTemplate;
+
+import javax.validation.constraints.AssertFalse;
+import java.util.Arrays;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //@TestPropertySource(locations = "/application-test.properties",
@@ -18,10 +30,42 @@ public class UsersControllerIntegrationTest{
     @LocalServerPort
     private int localServerPort;
 
-    @Test
-    void contextLoads(){
-        System.out.println(" defining port in properties and using @SBT annotation");
-        System.out.println("server port: " + serverPort);
-        System.out.println("local port: " + localServerPort);
-    }
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+   @Test
+   @DisplayName("user can be created")
+    void testCreateUser_whenValidDetailsProvided_returnsUserDetails() throws JSONException {
+       //Arrange
+       JSONObject userDetailsRequestJson = new JSONObject();
+       userDetailsRequestJson.put("firstName","Timz");
+       userDetailsRequestJson.put("lastName","Owen");
+       userDetailsRequestJson.put("email","timz@gmail.com");
+       userDetailsRequestJson.put("password","12345678");
+       userDetailsRequestJson.put("repeatPassword","12345678");
+
+       HttpHeaders headers = new HttpHeaders();
+       headers.setContentType(MediaType.APPLICATION_JSON);
+       headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+       HttpEntity<String> request = new HttpEntity<>(userDetailsRequestJson.toString(), headers);
+
+       // Act
+       ResponseEntity<UserRest> createdUserDetailsEntity = testRestTemplate.postForEntity(
+               "/users",
+               request,
+               UserRest.class);
+       UserRest createdUserDetails = createdUserDetailsEntity.getBody();
+
+       //Assert
+       Assertions.assertEquals(HttpStatus.OK, createdUserDetailsEntity.getStatusCode());
+       Assertions.assertEquals(userDetailsRequestJson.getString("firstName"),
+               createdUserDetails.getFirstName(),"Returned user's firstName seems to be incorrect");
+       Assertions.assertEquals(userDetailsRequestJson.getString("lastName"),
+               createdUserDetails.getLastName(),"Returned user's last name seems to be incorrect");
+       Assertions.assertEquals(userDetailsRequestJson.getString("email"),
+               createdUserDetails.getEmail(),"Returned user's email seems to be incorrect");
+       Assertions.assertFalse(createdUserDetails.getUserId().trim().isEmpty(),"user Id should not be empty");
+
+   }
 }
