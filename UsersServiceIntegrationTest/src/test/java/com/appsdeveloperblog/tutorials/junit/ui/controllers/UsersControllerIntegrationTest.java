0d6f2c497f80eaba9c4914028426a2c1cfa4703b
TimzOwen <timzowen@gmail.com>
2024-02-07 11:38:05 +0300

    definition of port numbers

---
+++ UsersServiceIntegrationTest/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.tutorials.junit.ui.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = "server.port=8081")
+public class UsersControllerIntegrationTest {
+
+    @Test
+    void contextLoads(){
+        System.out.println(" defining port in properties and using @SBT annotation");
+    }
+}
