65a4ca9f26be6b05ea86cbdc52c7d1b402026c3d
TimzOwen <timzowen@gmail.com>
2024-02-11 22:11:31 +0300

    unit tests

---
+++ hospital-crud/src/test/java/com/timzowen/hospital/crud/controller/DoctorsControllersTest.java
@@ -0,0 +1,14 @@
+package com.timzowen.hospital.crud.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoctorsControllersTest {
+
+    @Test
+    void testCreateDoctor_whenRightDetailsProvided_returnDoctor(){
+        System.out.println("return data once student saved");
+    }
+
+}
