cc8275a48d34708e273fd93e4831c1fc40f19362
TimzOwen <timzowen@gmail.com>
2024-02-13 22:54:50 +0300

    packing struture

---
+++ Employeecrudapi/src/main/java/com/timzowen/crudapi/service/EmployeeService.java
@@ -0,0 +1,4 @@
+package com.timzowen.crudapi.service;
+
+public interface EmployeeService {
+}
---
+++ Employeecrudapi/src/main/java/com/timzowen/crudapi/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,6 @@
+package com.timzowen.crudapi.service.impl;
+
+import com.timzowen.crudapi.service.EmployeeService;
+
+public class EmployeeServiceImpl implements EmployeeService {
+}
